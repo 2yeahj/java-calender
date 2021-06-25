77b06ca9c2888b4ff87b24036038188c65f102b8
yeahjjjj <20190930@sungshin.ac.kr>
2021-06-25 11:37:17 +0900

    calendar with while, break, continue

--- src/calendar.java
+++ src/calendar.java
@@ -9,12 +9,27 @@ public class calendar {
 	public static void main(String[] args) {
 		
 		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		
+		String PROMPT="cal> ";
 		Scanner scanner = new Scanner(System.in);
 		calendar cal = new calendar();
-		System.out.println("달을 입력해주세요.");
-		int month = scanner.nextInt();
 		
-		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.maxdaysOfMonth(month));
+		int month=1;
+		
+		while (true) {
+			System.out.println("달을 입력해주세요.");
+			System.out.print(PROMPT);
+			month = scanner.nextInt();
+			if (month == -1) {
+				break;
+			}
+			if (month > 12) {
+				continue;
+			}
+			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.maxdaysOfMonth(month));
+		}	
+		
+		System.out.println("Bye!");
 		scanner.close();
 	}
 }
